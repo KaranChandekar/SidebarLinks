b151a0e16df3c68a849f3398cb7338d9114afd71
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-12 20:19:29 +0530

    select elements

---
+++ app.js
@@ -0,0 +1,4 @@
+// select elements 
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
