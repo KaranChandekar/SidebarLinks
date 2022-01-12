b3c6505a3cfca28894cdc44ff9a0e50fb0128b6b
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-12 20:34:26 +0530

    one liner toggle

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
 
 toggleBtn.addEventListener("click", function () {
-  if (sidebar.classList.contains("show-sidebar")) {
-    sidebar.classList.remove("show-sidebar");
-  } else {
-    sidebar.classList.add("show-sidebar");
-  }
+  //   if (sidebar.classList.contains("show-sidebar")) {
+  //     sidebar.classList.remove("show-sidebar");
+  //   } else {
+  //     sidebar.classList.add("show-sidebar");
+  //   }
+
+  // one liner toggle
+  sidebar.classList.toggle("show-sidebar");
 });
